9c72518484901aaa6e791a23a8d073b0d977eff6
Mykola <nickkosh@gmail.com>
2023-03-03 16:39:40 +0200

    add spinner

--- app/src/main/java/com/example/healthdiary/DiseasesActivity.kt
+++ app/src/main/java/com/example/healthdiary/DiseasesActivity.kt
@@ -2,10 +2,37 @@ package com.example.healthdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.example.healthdiary.databinding.ActivityMainBinding
 
 class DiseasesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diseases)
+
+        val parts = resources.getStringArray(R.array.Parts)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, parts)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@DiseasesActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + parts[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/healthdiary/MainActivity.kt
+++ app/src/main/java/com/example/healthdiary/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.healthdiary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.healthdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
